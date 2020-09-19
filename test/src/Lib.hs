2a5a9751402ff76c9c2710104e725eee7e82589f
Joona Piirainen <joona.piirainen@gmail.com>
2020-09-19 23:24:18 +0300

    playing

--- test/src/Lib.hs
+++ test/src/Lib.hs
@@ -3,4 +3,23 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "Hello world!"
+
+add :: Num a => a -> a -> a
+add x y = x + y
+
+double x = x + x
+
+quadruple x = double ( double x )
+
+factorial n = product [1 .. n]
+
+average ns = sum ns `div` length ns
+
+poly x = let
+    y = y + 1
+    in y * y
+
+getEvens xs = filter (\x -> x `mod` 2 == 0) xs
+
+
