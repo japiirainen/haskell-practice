0da93b90f579b15b19a4294ea60fa3070459f6c1
japiirainen <joona.piirainen@gmail.com>
2020-11-02 21:27:43 +0200

    fizzbuzz

--- codecatas/FizzBuzz.hs
+++ codecatas/FizzBuzz.hs
@@ -1,9 +1,11 @@
 module FizzBuzz where
 
-import prelude ()
-
 fizzbuzz :: Int -> String
 fizzbuzz n | n `mod` 15 = "fizzBuzz"
            | n `mod` 5 = "fizz"
            | n `mod` 3 = "fizz"
            | otherwise = show n
+
+
+main :: IO()
+main :: mapM_ putStrLn $ map fizzbuzz [1..100]
