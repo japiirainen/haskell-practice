9e3b4705139d760d7da9f514be94bd91f47ff4e8
japiirainen <joona.piirainen@gmail.com>
2020-11-09 19:32:09 +0200

    common-functors

--- functors/common-functors.hs
+++ functors/common-functors.hs
@@ -29,5 +29,31 @@ greetUser x =
                     Just a -> a
                     Nothing -> T.pack "no user found with that id"
 
+
+data Pair a = Pair a a deriving Show
+
+instance Functor Pair where
+    fmap f (Pair l a) = Pair (f l) (f a)
+
+data Username a = Username a a deriving Show
+instance Functor Username where
+    fmap f (Username l a) = Username (f l) (f a)
+
+user :: Username String
+user = Username " Julie " "  Modonuki"
+
+users :: [Username String]
+users = [Username " Julie " "Moronuki", Username "Chris " " Martin"]
+
+stripSpaces :: Username String -> Username String
+stripSpaces username = fmap (T.unpack . T.strip . T.pack) username
+
+stripUsers :: [Username String] -> [Username String]
+stripUsers = Prelude.map stripSpaces
+
+instance Functor ((,,) a b) where
+    fmap f (x, y, a) = (x, y, f a)
+
+
 main :: IO ()
 main = undefined
