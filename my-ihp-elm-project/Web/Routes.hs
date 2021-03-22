bb0caab6ea06ed71bc4807150ff0791ad8dda22f
Joona Piirainen <joona.piirainen@gmail.com>
2021-03-22 19:39:59 +0200

    got ihp to work

---
+++ my-ihp-elm-project/Web/Routes.hs
@@ -0,0 +1,10 @@
+module Web.Routes where
+import           Generated.Types
+import           IHP.RouterPrelude
+import           Web.Types
+
+-- Generator Marker
+instance AutoRoute StaticController
+
+instance AutoRoute PostsController
+
