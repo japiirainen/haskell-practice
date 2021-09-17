bfb9aca5285360a796911b90f9f44cc4ce340bf9
Joona Piirainen <joona.piirainen@gmail.com>
2021-09-17 11:42:12 +0300

    add StatT

--- monad-transformers/app/Main.hs
+++ monad-transformers/app/Main.hs
@@ -130,6 +130,41 @@ eval3 (App e1 e2) = do
         _                  -> throwError "Type error in application"
 
 
+type Eval4 a = ReaderT Env (ExceptT String (StateT Integer Identity)) a
+runEval4 :: Env -> Integer -> Eval4 a -> (Either String a, Integer)
+runEval4 env st ev = runIdentity $ runStateT (runExceptT (runReaderT ev env)) st
+
+tick :: (Num s, MonadState s m) => m ()
+tick = modify (+ 1)
+
+-- >>> runEval4 Map.empty 0 (eval4 exampleExp)
+eval4 :: Exp -> Eval4 Value
+eval4 (Lit i) = do {tick; return $ IntVal i}
+eval4 (Var n) = do
+    tick
+    env <- ask
+    case Map.lookup n env of
+        Just val -> return val
+        Nothing  -> throwError $ "unbound variable: " ++ n
+eval4 (Plus e1 e2) = do
+    tick
+    e1' <- eval4 e1
+    e2' <- eval4 e2
+    case (e1', e2') of
+        (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+        _                      -> throwError "Type error in addition"
+eval4 (Abs n e) = do
+    tick
+    env <- ask
+    return $ FunVal env n e
+eval4 (App e1 e2) = do
+    tick
+    val1 <- eval4 e1
+    val2 <- eval4 e2
+    case val1 of
+        FunVal env' n body -> local (const $ Map.insert n val2 env') $ eval4 body
+        _                  -> throwError "Type error in application"
+
 exampleExp :: Exp
 exampleExp = Lit 12 `Plus` App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2)
 
