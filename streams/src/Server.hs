4fc9c0884034f2c0747c225b9ae9d4f9e1513b95
Joona Piirainen <joona.piirainen@gmail.com>
2021-10-13 11:06:27 +0300

    streams

---
+++ streams/src/Server.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections       #-}
+
+module Server (main) where
+
+import           Control.Monad                     (void)
+import           Control.Monad.Catch               (SomeException, catch)
+import           Data.Char                         (isSpace)
+import           Data.Function                     ((&))
+import           Network.Socket                    (Socket)
+import           Streamly.Data.Fold                (Fold)
+import           System.Random                     (randomIO)
+
+import qualified Data.Map.Strict                   as Map
+import qualified Streamly.Data.Array.Foreign       as Array
+import qualified Streamly.Data.Fold                as Fold
+import qualified Streamly.Network.Inet.TCP         as TCP
+import qualified Streamly.Network.Socket           as Socket
+import qualified Streamly.Prelude                  as Stream
+import qualified Streamly.Unicode.Stream           as Unicode
+
+import qualified Streamly.Internal.Data.Fold       as Fold (demuxDefault)
+import qualified Streamly.Internal.Data.Time.Clock as Clock (Clock (..),
+                                                             getTime)
+
+sendValue :: Show a => Socket -> a -> IO ()
+sendValue sk x =
+      Stream.fromList (show x ++ "\n")
+    & Unicode.encodeLatin1
+    & Stream.fold (Array.writeN 60)
+    >>= Socket.writeChunk sk
+
+time :: Socket -> IO ()
+time sk = Clock.getTime Clock.Monotonic >>= sendValue sk
+
+random :: Socket -> IO ()
+random sk = (randomIO :: IO Int) >>= sendValue sk
+
+def :: (String, Socket) -> IO ()
+def (str, sk) = sendValue sk $ "Unknown command: " <> str
+
+commands :: Map.Map String (Fold IO Socket ())
+commands = Map.fromList
+    [ ("time", Fold.drainBy time)
+    , ("random", Fold.drainBy random)
+    ]
+
+demux :: Fold IO (String, Socket) ()
+demux = snd <$> Fold.demuxDefault commands (Fold.drainBy def)
+
+main :: IO ()
+main = undefined
