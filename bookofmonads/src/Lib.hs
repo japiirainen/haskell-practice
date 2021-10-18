43f4c7db06de4fca72b5517e09a1c98e359e9a9b
Joona Piirainen <joona.piirainen@gmail.com>
2021-10-18 21:36:59 +0300

    Identity

--- bookofmonads/src/Lib.hs
+++ bookofmonads/src/Lib.hs
@@ -1,6 +1,23 @@
 module Lib
     ( someFunc
     ) where
+import           Control.Applicative (Applicative (liftA2))
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+newtype Identity a = I a
+
+instance Functor Identity where
+    fmap f (I a) = I $ f a
+
+
+instance Applicative Identity where
+    liftA2 f (I a) (I b) = I $ f a b
+    (I a) <*> b = fmap a b
+    pure = I
+
+instance Monad Identity where
+    return = I
+    (I a) >>= f = f a
