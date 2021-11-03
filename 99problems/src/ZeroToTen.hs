5a94676fefd1cc93100b284f1d68f91109c37fa6
Joona Piirainen <joona.piirainen@gmail.com>
2021-11-03 17:18:39 +0200

    8 first problems

--- 99problems/src/ZeroToTen.hs
+++ 99problems/src/ZeroToTen.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE DeriveAnyClass #-}
 module ZeroToTen where
 
-import           Prelude hiding (last)
+import qualified Data.List as List
+import           Prelude   hiding (last, length, reverse)
+import qualified Prelude
 
 {-|
+No.1
 >>> last [1, 2, 3]
 >>> last []
 Just 3
@@ -15,6 +19,7 @@ last (_:xs) = last xs
 
 
 {-|
+No.2
 >>> butLast [1, 2, 3, 4]
 >>> butLast ['a' .. 'z']
 >>> butLast []
@@ -23,7 +28,81 @@ Just 'y'
 Nothing
 -}
 butLast :: [a] -> Maybe a
-butLast xs = case (take 2 . reverse) xs of
+butLast xs = case (take 2 . Prelude.reverse) xs of
     (x:y:_) -> Just y
     _       -> Nothing
 
+
+{-|
+No.3
+>>> elementAt [1, 2, 3] 2
+>>> elementAt "haskell" 5
+Just 2
+Just 'e'
+-}
+elementAt :: [a] -> Int -> Maybe a
+elementAt xs n =
+    if n > Prelude.length xs
+        then Nothing
+        else Just $ (fst . Prelude.last) $ zip xs [1 .. n]
+
+{-|
+No.4
+>>> length [1, 2, 3]
+>>> length "haskell"
+-}
+length :: [a] -> Int
+length []     = 0
+length (_:xs) = 1 + length xs
+
+{-|
+No.5
+>>> reverse [1, 2, 3]
+>>> reverse "haskell"
+[3,2,1]
+"lleksah"
+-}
+reverse :: [a] -> [a]
+reverse = foldl (flip (:)) []
+
+{-|
+No.6
+>>> isPalindrome "madamimadam"
+>>> isPalindrome [1, 2, 3, 2, 1]
+>>> isPalindrome [1, 2, 3]
+True
+True
+False
+-}
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = xs == reverse xs
+
+{-|
+No.7
+>>> flatten (Elem 5)
+>>> flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
+>>> flatten (List [])
+[5]
+[1,2,3,4,5]
+[]
+-}
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem x)  = [x]
+flatten (List xs) = mconcat $ map flatten xs
+
+flatten' :: NestedList a -> [a]
+flatten' (Elem x)  = return x
+flatten' (List xs) = flatten' =<< xs
+
+{-|
+No.8
+>>> compress "aaaabccaadeeee"
+>>> compress [1,2,2,2,3,3,3,4]
+"abcade"
+[1,2,3,4]
+-}
+
+compress :: Eq a => [a] -> [a]
+compress = (<$>) List.head . List.group
